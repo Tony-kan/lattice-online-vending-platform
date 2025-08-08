19ad8a8e67607c0c347a8d2341dae00cde713390
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-08-08 21:31:56 +0200

    Frontend setup : install packages,configure dependencies

---
+++ ovp-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center border w-full h-screen">
+        <h1>Online vending platform</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
